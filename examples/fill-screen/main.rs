6f879739b685f5dfab86bf518732129f1ee9e682
Steven Rémot <steven.remot@gmail.com>
2018-05-01 15:23:39 +0200

    Basic API for filling the screen with a color

---
+++ examples/fill-screen/main.rs
@@ -0,0 +1,16 @@
+extern { fn graphics_fill(r: u32, g: u32, b: u32); }
+
+#[no_mangle]
+pub unsafe extern fn init() {
+
+}
+
+static mut count: u32 = 0;
+
+#[no_mangle]
+pub unsafe extern fn update(step: u32) {
+    count += step;
+    let time: f32 = (count as f32) / 1000.0;
+    let red: u32 = (((time.sin() + 1.0) / 2.0) * 255.0) as u32;
+    graphics_fill(red, 0, 0);
+}
---
+++ gzel.js
@@ -0,0 +1,90 @@
+const SCREEN_WIDTH = 240;
+const SCREEN_HEIGHT = 140;
+
+// Graphics
+function toHexPart(val) {
+  return val.toString(16).padStart(2, "0");
+}
+
+class Renderer {
+  constructor(target) {
+    const canvas = document.createElement("canvas");
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    document.querySelector(target).appendChild(canvas);
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+  }
+
+  fill(r, g, b) {
+    this.ctx.save();
+    this.ctx.fillStyle = "#" + [r, g, b].map(toHexPart).join("");
+    this.ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    this.ctx.restore();
+  }
+
+  exportAPI() {
+    return {
+      graphics_fill: this.fill.bind(this)
+    };
+  }
+}
+
+// Main API
+const fps = 60;
+const iterStep = 1000 / fps;
+
+export class Console {
+  constructor(target) {
+    this.renderer = new Renderer(target);
+    this.cartridge = null;
+    this.lastIterTime = null;
+    this.timeCount = 0;
+  }
+
+  exportAPI() {
+    return Object.assign(
+      {
+        debug_log(r) {
+          console.log(r);
+        },
+        sinf: Math.sin
+      },
+      this.renderer.exportAPI()
+    );
+  }
+
+  load(fileUrl) {
+    return fetch(fileUrl)
+      .then(response => response.arrayBuffer())
+      .then(wasm => WebAssembly.instantiate(wasm, { env: this.exportAPI() }))
+      .then(cartridge => {
+        this.cartridge = cartridge;
+        this.start();
+      });
+  }
+
+  start() {
+    this.lastIterTime = null;
+    this.timeCount = 0;
+    this.cartridge.instance.exports.init();
+    this.loop();
+  }
+
+  loop() {
+    requestAnimationFrame(time => {
+      if (this.lastIterTime !== null) {
+        const delay = time - this.lastIterTime;
+        this.timeCount += delay >= 1000 ? 0 : delay;
+
+        while (this.timeCount > iterStep) {
+          this.cartridge.instance.exports.update(iterStep);
+          this.timeCount -= iterStep;
+        }
+      }
+
+      this.lastIterTime = time;
+      this.loop();
+    });
+  }
+}
